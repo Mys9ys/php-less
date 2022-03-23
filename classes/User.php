2b885b1551deeac4d8d6c49dd5ac6769accda601
mys9ys <rjbexa@yandex.ru>
2022-03-23 13:50:07 +0300

    less 3

--- classes/User.php
+++ classes/User.php
@@ -12,4 +12,34 @@ class User
     {
         $this->name = $name; // запишем новое значение свойства name
     }
+
+    // Метод для проверки возраста:
+    public function isAgeCorrect($age)
+    {
+        if ($age >= 18 and $age <= 60) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Метод для изменения возраста юзера:
+    public function setAge($age)
+    {
+        // Проверим возраст на корректность:
+        if ($this->isAgeCorrect($age)) {
+            $this->age = $age;
+        }
+    }
+
+    // Метод для добавления к возрасту:
+    public function addAge($years)
+    {
+        $newAge = $this->age + $years; // вычислим новый возраст
+
+        // Проверим возраст на корректность:
+        if ($this->isAgeCorrect($newAge)) {
+            $this->age = $newAge; // обновим, если новый возраст прошел проверку
+        }
+    }
 }
